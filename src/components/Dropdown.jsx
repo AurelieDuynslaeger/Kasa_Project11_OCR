b5a2e5991538127f0c20a05a5f804fffc72a3e10
AureLeeLi <aduynslaeger@gmail.com>
2024-08-10 13:27:35 +0200

    ✅ dropdown component done + 🎨  styling to come + 💡react-icons install

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,47 @@
-const Dropdown = () => {
+import { useState } from "react"
+import PropTypes from "prop-types"
+import "../stylesheet/main.scss"
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+
+const Dropdown = ({ title, content, items }) => {
+    //état d'ouverture du dropdown
+    const [isOpen, setIsOpen] = useState(false);
+
+    //déclencheur du dropdown
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+      };
     return (
-        <div>Dropdown</div>
+        <div className="dropdown">
+            <button onClick={toggleDropdown} className="dropdown_btn">
+                {title}
+                <span className={isOpen ? "arrowDown" : "arrowUp"}>
+                    {isOpen ?  <IoIosArrowDown /> : <IoIosArrowUp />}
+                </span>
+            </button>
+            {isOpen && (
+                <div className="dropdown_content">
+                {/* conditionnement si c'est juste un paragraphe ou un tableau d'élements */}
+                {content && <p>{content}</p>}
+                {items && (
+                    <ul>
+                        {items.map((item, index) => (
+                            <li key={index} className="dropdown_item">
+                            {item}
+                            </li>
+                ))}
+                    </ul>    
+                )}
+                </div>
+            )}
+    </div>
     )
 }
+
+Dropdown.propTypes = {
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.string),
+  };
 
 export default Dropdown
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,21 +1,48 @@
-import Footer from "../../components/Footer/Footer"
+import Dropdown from "../../components/Dropdown"
 import Header from "../../components/Header/Header"
 import ImgBanner from "../../components/ImgBanner/ImgBanner"
+import Footer from "../../components/Footer/Footer"
 import "./about.scss"
+
+const about_p = [
+  {
+    title : "Fiabilité", 
+    paragraph : "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+  },
+  {
+    title : "Respest", 
+    paragraph : "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+  },
+  {
+    title : "Service", 
+    paragraph : "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+  },
+  {
+    title : "Sécurité", 
+    paragraph : "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+  },
+]
 
 const About = () => {
   return (
     <div className='container'>
-        <Header/>
-        <ImgBanner 
-        imageUrl="../src/assets/banner_about.png" 
-        title=""/>
-        <div className="main">
-          
-        </div>
-        <Footer/>
+      <Header />
+      <ImgBanner
+        imageUrl="../src/assets/banner_about.png"
+        title=""
+      />
+      <div className="main">
+        {about_p.map((item, index) => (
+          <Dropdown
+            key={index}
+            title={item.title}
+            content={item.paragraph}
+          />
+        ))}
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default About
