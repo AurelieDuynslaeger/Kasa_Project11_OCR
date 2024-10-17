d858576dd4f9f9c5fc4be019c98625e18d7dccd2
AureLeeLi <aduynslaeger@gmail.com>
2024-10-17 12:59:37 +0200

    fix path router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AppRouter from './Router/AppRouter.jsx';
+import AppRouter from './router/AppRouter';
 import './stylesheet/main.scss'
 
 createRoot(document.getElementById('root')).render(
